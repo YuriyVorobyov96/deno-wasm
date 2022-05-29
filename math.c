9f4a149aa00a6fcf74059e87f0aa697691b6cd64
YuriyVorobyov96 <darthrevan1333@gmail.com>
2022-05-29 11:24:53 +0300

    refactored math.c

--- math.c
+++ math.c
@@ -1,11 +1,15 @@
 #include <emscripten.h>
 
+/* 
+ * EMSCRIPTEN_KEEPALIVE - a macro to expose function for using in JS/TS
+ */
+
 EMSCRIPTEN_KEEPALIVE
 int add(int x, int y) {
   return x + y;
 }
 
 EMSCRIPTEN_KEEPALIVE
-int subtract(int x, int y){
-  return x-y;
+int subtract(int x, int y) {
+  return x - y;
 }
